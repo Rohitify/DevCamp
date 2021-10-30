9bec8c5cadb2e9eda3bd4354d0e6fb3063bb6d05
Rohit Singh <rhtsing@gmail.com>
2021-10-31 01:22:52 +0530

    Connect DB

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+// Routes 
 const bootcampsRoutes = require("./routes/bootcamps");
 
 //Load ENV variables
 dotenv.config({ path : "./config/config.env" });
 
+// Connect to database
+connectDB();
+
 const app = express();
+
+// Dev logging middleware
+if(process.env.NODE_ENV === "development"){
+  app.use(morgan("dev"));
+}
 
 app.use("/api/v1/bootcamps", bootcampsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle the Unhandled Error 
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error : ${err.message}`);
+  // Close server
+  server.close(() => process.exit(1));
+});
