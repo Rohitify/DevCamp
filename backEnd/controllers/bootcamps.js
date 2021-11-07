ec688ea7588d151d35762ed7c53a4a1e1857d6d8
Rohit Singh <rhtsing@gmail.com>
2021-11-07 15:03:36 +0530

    using query operator

--- backEnd/controllers/bootcamps.js
+++ backEnd/controllers/bootcamps.js
@@ -4,7 +4,13 @@ const ErrorResponse = require("../utils/errorResponse");
 const geocoder = require("../utils/geocoder");
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    const bootcamps = await Bootcamp.find();
+  let queryStr;
+  queryStr = JSON.stringify(req.params);
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  const query = Bootcamp.find(JSON.parse(queryStr));
+
+    const bootcamps = await query;
 
     res.status(200).json({
       success: true,
@@ -92,3 +98,4 @@ exports.getBootcampsInRadius = asyncHandler( async (req, res, next) => {
     data: bootcamps
   });
 } );
+ 
