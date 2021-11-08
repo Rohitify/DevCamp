880095ab82e71d7ea8d4ed78eb5fc4ce71b98e25
Rohit Singh <rhtsing@gmail.com>
2021-11-08 21:15:34 +0530

    add average cost to every bootcamp

--- backEnd/models/Course.js
+++ backEnd/models/Course.js
@@ -38,4 +38,36 @@ const CourseSchema = new mongoose.Schema({
   }
 });
 
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+  console.log("Calculating avg cost...");
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp : bootcampId }
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" }
+      }
+    }
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost /10) * 10
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+CourseSchema.post("save", function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+CourseSchema.pre("remove", function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("course", CourseSchema);
