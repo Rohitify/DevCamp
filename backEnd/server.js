54d028cc29cc656d154369d17cfd813293ed7403
Rohit Singh <rhtsing88@gmail.com>
2024-03-16 23:27:27 +0530

    Update server.js
    moving frontend build to backend

--- backEnd/server.js
+++ backEnd/server.js
@@ -82,10 +82,10 @@ app.use("/api/v1/reviews", reviewsRoutes);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, './build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
